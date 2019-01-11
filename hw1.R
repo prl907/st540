6beebde2997088dfc9499a2a66834c9036a75055
robin <prl90777@gmail.com>
2019-01-10 23:23:40 -0500

    remove loop from question 1a, and used unlist

--- hw1.R
+++ hw1.R
@@ -3,17 +3,15 @@ library(ggplot2)
 
 
 #import file from desktop
-ozone<- read.csv("C:/Users/prl90/Documents/ST540/ozone.csv", header=T, na.rm = T)
+ozone<- read.csv("C:/Users/prl90/Documents/st540/ozone.csv", header=T)
 
 r <- nrow(ozone)
 c <- ncol(ozone)
 
-str(ozone)
-
-ozoneR[1,1]
-
 
 ozoneR<- ozone[2:ncol(ozone)]
+
+
 data<- rep(NA,nrow(ozoneR) * ncol(ozoneR) )
 count = 1;
 for(i in 1:nrow(ozoneR)){
@@ -23,10 +21,11 @@ for(i in 1:nrow(ozoneR)){
     
   }
 }
-
+data<- unlist(ozoneR)
+#statistics
 mean(data, na.rm = T)
-std<- apply(data,2,function(x)sd(x, na.rm = T))
-pMiss<- apply(data, 2, function(x)(sum(1 * is.na(x))/length(data)))
+sd(data, na.rm = T)
+sum(1 * is.na(data))/length(data)
 
 
 #part b
@@ -49,10 +48,9 @@ for(i in 1:r){
   }
 }
 
-df<- map(list(mPlot,sPlot, pPlot), ~data.frame(x= .x))
-dfC<- list("green", "red", "blue")
 
-map2(df,dfC, ~(ggplot()+ geom_histogram(data = ..1, bins = 5,aes(x = ..1$x),  fill = ..2 )))
+
+walk(list(mPlot, sPlot, pPlot), hist)
 
 
 
